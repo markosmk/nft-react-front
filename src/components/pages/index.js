b491c35b0f6bec0753e3d356d799be54a9633acf
Markos <markosmk@me.com>
2021-12-16 22:00:21 -0300

    add - to manage everything in one import

---
+++ src/components/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as ItemNft } from './ItemNft';
+export { default as Owner } from './Owner';
+export { default as NotFound } from './NotFound';
